f99a513e862b696d4666168a0793201a8555053c
korven48 <korven48@gmail.com>
2021-09-22 16:14:15 +0200

    Add files via upload

---
+++ src/es/ucm/tp1/Coin.java
@@ -0,0 +1,5 @@
+package es.ucm.tp1;
+
+public class Coin {
+	private int x, y;
+}
---
+++ src/es/ucm/tp1/Obstacle.java
@@ -0,0 +1,10 @@
+package es.ucm.tp1;
+
+public class Obstacle {
+	private int x, y;
+	private Game game;
+	
+	public int add(int x, int y) {
+		return x + y;
+	}
+}
