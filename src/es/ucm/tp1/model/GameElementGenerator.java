c3e010a97c699c83aa81161435ba80fa3e0ac23e
SoftwareStackLukas <83038634+SoftwareStackLukas@users.noreply.github.com>
2021-11-02 18:40:29 +0100

    Add files via upload

---
+++ src/es/ucm/tp1/model/GameElementGenerator.java
@@ -0,0 +1,19 @@
+package es.ucm.tp1.model;
+
+import es.ucm.tp1.control.Level;
+
+public final class GameElementGenerator {
+	public static void generateGameElements(Game game, Level level) {
+		//Have to import set the functions correct
+		for(int x = game.getVisibility()/2; x < game.getRoadLength(); x ++) {
+			game.tryToAddObject(new Obstacle(game, x, game.getRandomLane()), level.obstacleFrequency());
+			game.tryToAddObject(new Coin(game, x, game.getRandomLane()), level.coinFrequency());
+		}
+	}
+	
+	public static void reset(Level level) {
+		//This methods just contain a header nothing more
+		Obstacle.reset();
+		Coin.reset();
+	}
+}
