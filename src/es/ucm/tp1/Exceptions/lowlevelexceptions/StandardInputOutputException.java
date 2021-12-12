9d4eaf9ba2533215edd26b28c56eb86c9aa7395e
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2021-12-12 18:07:50 +0100

    Extra Exception

---
+++ src/es/ucm/tp1/Exceptions/lowlevelexceptions/StandardInputOutputException.java
@@ -0,0 +1,30 @@
+package es.ucm.tp1.Exceptions.lowlevelexceptions;
+
+public class StandardInputOutputException extends Exception {
+
+	public StandardInputOutputException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StandardInputOutputException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StandardInputOutputException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StandardInputOutputException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StandardInputOutputException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
